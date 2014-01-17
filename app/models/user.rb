f081b6e177c5dc89e9a4c17a5d60a901ae5792b1
Geoff Crittenden <geoffcritt@hotmail.com>
2014-01-17 09:31:17 -0600

    added fields to Users migraton and validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+
+  validates :username, presence: true
+  validates :type, presence: true
+  validates :organization_id, presence: true
+  validates :password_digest, presence: true
+
 end
--- db/migrate/20140117145057_create_users.rb
+++ db/migrate/20140117145057_create_users.rb
@@ -1,7 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.
+      t.string  :username
+      t.string  :first_name
+      t.string  :last_name
+      t.string  :type
+      t.integer :organization_id
+      t.string  :password_digest
 
       t.timestamps
     end
