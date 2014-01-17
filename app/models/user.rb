8318ce6e73ea8cba7f0f3d48e5e3f6923f83c010
Geoff Crittenden <geoffcritt@hotmail.com>
2014-01-17 09:39:49 -0600

    added BCrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,23 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  include BCrypt
+
+  has_secure_password
 
   validates :username, presence: true
   validates :type, presence: true
   validates :organization_id, presence: true
   validates :password_digest, presence: true
+  validates :password_confirmation, presence: true
+
+  def password
+    @password ||= Password.new(password_digest)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_digest = @password
+  end
 
 end
