081ce501a2e1bf75847135ed07c7b1b960668402
Geoff Crittenden <geoffcritt@hotmail.com>
2014-01-18 13:59:03 -0600

    troubleshooting associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
 
   belongs_to :restaurant, class_name: 'Organization'
-  has_many :customers, class_name: 'User'
+  has_many :customers, foreign_key: 'id', class_name: 'User'
 
   validates :name, presence: true, length: { minimum: 1 }
   validates :description, presence: true, length: { minimum: 1 }
