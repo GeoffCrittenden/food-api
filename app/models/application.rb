8cc545be8c573511ba9d13d27a742b5be7b1f22d
Geoff Crittenden <geoffcritt@hotmail.com>
2014-01-18 10:16:02 -0600

    updated application model

--- app/models/application.rb
+++ app/models/application.rb
@@ -5,4 +5,8 @@ class Application < ActiveRecord::Base
   has_many :users
   has_many :admins, foreign_key: 'admin_id', class_name: 'User'
 
+  def create_auth_token
+    @auth_token = Base64.encode64(self.name)    
+  end
+
 end
