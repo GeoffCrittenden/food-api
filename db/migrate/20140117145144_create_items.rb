2bf61570c039b10a8387a83bbbd5cc167efadaaf
Geoff Crittenden <geoffcritt@hotmail.com>
2014-01-18 09:37:25 -0600

    removed restaurants migration and added fields to items table

--- db/migrate/20140117145144_create_items.rb
+++ db/migrate/20140117145144_create_items.rb
@@ -1,6 +1,9 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
+      t.string  :name
+      t.string  :description
+      t.integer :restaurant_id
 
       t.timestamps
     end
