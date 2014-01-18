4c33d1c9cd9c118d9da50d74c7c950fc10a01591
Geoff Crittenden <geoffcritt@hotmail.com>
2014-01-18 09:06:57 -0600

    defined Organization spec tests

--- spec/organization_spec.rb
+++ spec/organization_spec.rb
@@ -2,6 +2,19 @@ require 'spec_helper'
 
 describe Organization do
 
-  
+  it "should open a new Organization" do
+  end
+
+  it "should create a new Organization" do
+  end
+
+  it "requires an organization name" do
+  end
+
+  it "requires an organization type" do
+  end
+
+  it "requires the organization type to be restaurant, customer, or application" do
+  end
 
 end
