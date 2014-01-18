b903267eb32bd94aa9965e193441f0911e1870ac
Geoff Crittenden <geoffcritt@hotmail.com>
2014-01-18 09:21:42 -0600

    updated organization associations for customers

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,7 +1,7 @@
 class Organization < ActiveRecord::Base
 
-  has_many :users
   has_many :items
+  has_many :users, through :items, as :customers
 
   validates :name, presence: true
   validates :org_type, presence: true, inclusion: { in: %w(restaurant customer application) }
