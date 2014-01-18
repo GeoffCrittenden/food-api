14f813126cd472dd3da34d726a31ef685e71fe93
Geoff Crittenden <geoffcritt@hotmail.com>
2014-01-18 09:34:45 -0600

    updated organization associations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,8 +1,8 @@
 class Organization < ActiveRecord::Base
 
   has_many :items
-  has_many :users, through :items, as :customers
-  has_many :users, foreign_key :admin_id, as :admins
+  has_many :customers, through: :items, class_name: 'User'
+  has_many :admins, foreign_key: 'admin_id', class_name: 'User'
 
   validates :name, presence: true
   validates :org_type, presence: true, inclusion: { in: %w(restaurant customer) }
