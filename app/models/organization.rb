27540ddeddfd2367e67ea12fd4237da50b9308e8
Geoff Crittenden <geoffcritt@hotmail.com>
2014-01-18 08:56:32 -0600

    setting Organization migration and model validations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,10 @@
 class Organization < ActiveRecord::Base
+
+  has_many :users
+  has_many :items
+
+  validates :name, presence: true
+  validates :org_type, presence: true, inclusion: { in: %w(restaurant customer application) }
+  validates :admin_id, presence: true
+
 end
--- db/migrate/20140117145427_create_organizations.rb
+++ db/migrate/20140117145427_create_organizations.rb
@@ -1,6 +1,11 @@
 class CreateOrganizations < ActiveRecord::Migration
   def change
     create_table :organizations do |t|
+      t.string  :name
+      t.string  :address
+      t.string  :phone
+      t.string  :org_type
+      t.integer :admin_id
 
       t.timestamps
     end
