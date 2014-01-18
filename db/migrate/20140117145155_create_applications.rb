fe91eeb5b86f07ee597395963ae2dd0699a61ec0
Geoff Crittenden <geoffcritt@hotmail.com>
2014-01-18 09:52:01 -0600

    added fields to application table migration

--- db/migrate/20140117145155_create_applications.rb
+++ db/migrate/20140117145155_create_applications.rb
@@ -1,6 +1,9 @@
 class CreateApplications < ActiveRecord::Migration
   def change
     create_table :applications do |t|
+      t.string  :name
+      t.integer :admin_id
+      t.string  :auth_token
 
       t.timestamps
     end
