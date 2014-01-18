bcb63d8fdaa8404fe57b316efc20c07eab09d590
Geoff Crittenden <geoffcritt@hotmail.com>
2014-01-18 13:02:21 -0600

    added Item validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,8 @@ class Item < ActiveRecord::Base
   belongs_to :restaurant, class_name: 'Organization'
   has_many :customers, class_name: 'User'
 
+  validates :name, presence: true, length: { minimum: 1 }
+  validates :description, presence: true, length: { minimum: 1 }
+  validates :restaurant_id, presence: true
+
 end
