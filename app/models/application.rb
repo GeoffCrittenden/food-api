9219d9fa98e9d950793ba2b27c6a4365ad209dd9
Geoff Crittenden <geoffcritt@hotmail.com>
2014-01-18 11:08:10 -0600

    application model auth_token working

--- app/models/application.rb
+++ app/models/application.rb
@@ -5,8 +5,16 @@ class Application < ActiveRecord::Base
   has_many :users
   has_many :admins, foreign_key: 'admin_id', class_name: 'User'
 
+  after_initialize :create_auth_token
+
+  private
+
   def create_auth_token
-    @auth_token = Base64.encode64(self.name)    
+    if self.auth_token == nil
+      self.update_attributes(auth_token: Base64.encode64(self.name + User.find(self.admin_id).username).chop!)
+    else
+      puts 'This application already has an auth_token.'
+    end
   end
 
 end
