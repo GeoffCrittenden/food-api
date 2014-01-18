13e36dd5bd2dbbf130ecb141401115d4d4d8027f
Geoff Crittenden <geoffcritt@hotmail.com>
2014-01-18 13:35:01 -0600

    rspec tests written for Item

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,7 +1,38 @@
 require 'spec_helper'
 
 describe  Item do
+  before (:each) do
+    @items = Item.count
+    @item = { name: 'Chicken Burrito',
+              description: 'Burrito made with chicken.',
+              restaurant_id: 1 }
+  end
   
-  
+  it "opens a new Item" do
+    item = Item.new
+    item.should be_a(Item)
+  end
+
+  it "creates a new Item" do
+    Item.create(name: 'Twinkie',
+                description: 'Yellow sponge cake pastry snack filled with creme.',
+                restaurant_id: 1)
+    Item.count.should eql(@items + 1)
+  end
+
+  it "requires a name" do
+    Item.create(description: @item[:description], restaurant_id: @item[:restaurant_id])
+    Item.count.should eql(@items)
+  end
+
+  it "requires a description" do
+    Item.create(name: @item[:name], restaurant_id: @item[:restaurant_id])
+    Item.count.should eql(@items)
+  end
+
+  it "requires a restaurant id" do
+    Item.create(name: @item[:name], description: @item[:description])
+    Item.count.should eql(@items)
+  end
 
 end
