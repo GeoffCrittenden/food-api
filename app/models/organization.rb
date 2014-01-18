3a2997362afd5280ead6a00a214be4ba8578e502
Geoff Crittenden <geoffcritt@hotmail.com>
2014-01-18 09:25:09 -0600

    removed applications association with organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,8 +2,9 @@ class Organization < ActiveRecord::Base
 
   has_many :items
   has_many :users, through :items, as :customers
+  has_many :users, foreign_key :admin_id, as :admins
 
   validates :name, presence: true
-  validates :org_type, presence: true, inclusion: { in: %w(restaurant customer application) }
+  validates :org_type, presence: true, inclusion: { in: %w(restaurant customer) }
 
 end
