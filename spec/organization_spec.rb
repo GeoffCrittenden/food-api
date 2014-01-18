237b3f44a827c1778a7af844879e3e03479c763f
Geoff Crittenden <geoffcritt@hotmail.com>
2014-01-18 09:09:25 -0600

    first organization spec test

--- spec/organization_spec.rb
+++ spec/organization_spec.rb
@@ -3,18 +3,24 @@ require 'spec_helper'
 describe Organization do
 
   it "should open a new Organization" do
+    org = Organization.new
+    org.should be_a(Organization)
   end
 
   it "should create a new Organization" do
+    pending
   end
 
   it "requires an organization name" do
+    pending
   end
 
   it "requires an organization type" do
+    pending
   end
 
   it "requires the organization type to be restaurant, customer, or application" do
+    pending
   end
 
 end
