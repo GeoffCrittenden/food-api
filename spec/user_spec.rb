3db5ddd43625911e7f805a649ad85b8171e1a883
Geoff Crittenden <geoffcritt@hotmail.com>
2014-01-19 22:42:51 -0600

    refactored user_spec

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,18 +1,15 @@
 require 'spec_helper'
 
 describe User do
-  before do
+  before(:each) do
+    @users = User.count
     @user = { username: 'person@email.com',
               first_name: 'John',
               last_name: 'Doe',
               user_type: 'customer',
               organization_id: 1,
               password: 'password',
-              password_confirmation: 'password' }
-  end
-
-  before(:each) do
-    @users = User.count
+              password_confirmation: 'password' }  
   end
 
   it "should open a new User" do
@@ -26,105 +23,56 @@ describe User do
   end
 
   it "requires a username" do
-    users = User.count
-    user = User.create(first_name: 'This',
-                       last_name: 'Guy',
-                       user_type: 'customer',
-                       organization_id: 1,
-                       password: 'password',
-                       password_confirmation: 'password')
-    User.count.should eql(users)
+    @user.delete(:username)
+    user = User.create(@user)
+    User.count.should eql(@users)
   end
 
   it "requires the username to be an email address" do
-    users = User.count
-    user = User.create(username: 'username',
-                       first_name: 'This',
-                       last_name: 'Guy',
-                       user_type: 'customer',
-                       organization_id: 1,
-                       password: 'password',
-                       password_confirmation: 'password')
-    User.count.should eql(users)
+    @user[:username] = 'username'
+    user = User.create(@user)
+    User.count.should eql(@users)
   end
 
   it "requires a user type" do
-    users = User.count
-    user = User.create(username: 'person1@email.com',
-                       first_name: 'This',
-                       last_name: 'Guy',
-                       organization_id: 1,
-                       password: 'password',
-                       password_confirmation: 'password')
-    User.count.should eql(users)
+    @user.delete(:user_type)
+    user = User.create(@user)
+    User.count.should eql(@users)
   end
 
   it "requires the user type to be 'customer', 'restaurant', or 'app_admin'" do
-    users = User.count
-    user = User.create(username: 'person2@email.com',
-                       first_name: 'This',
-                       last_name: 'Guy',
-                       user_type: 'costumer',
-                       organization_id: 1,
-                       password: 'password',
-                       password_confirmation: 'password')
-    User.count.should eql(users)
+    @user[:user_type] = 'costumer'
+    user = User.create(@user)
+    User.count.should eql(@users)
   end
 
   it "requires an organization id" do
-    users = User.count
-    user = User.create(username: 'person3@email.com',
-                       first_name: 'This',
-                       last_name: 'Guy',
-                       user_type: 'customer',
-                       password: 'password',
-                       password_confirmation: 'password')
-    User.count.should eql(users)
+    @user.delete(:organization_id)
+    user = User.create(@user)
+    User.count.should eql(@users)
   end
 
   it "requires a password length of at least 8 characters" do
     pending "troubleshooting bcrypt validations"
-    users = User.count
-    user = User.create(username: 'person4@email.com',
-                       first_name: 'This',
-                       last_name: 'Guy',
-                       user_type: 'customer',
-                       organization_id: 1,
-                       password: 'passwor',
-                       password_confirmation: 'passwor')
-    User.count.should eql(users)
+    @user[:password] = 'passwor'
+    @user[:password_confirmation] = 'passwor'
+    user = User.create(@user)
+    User.count.should eql(@users)
   end
 
   it "requires a password confirmation" do
-    users = User.count
-    user = User.create(username: 'person5@email.com',
-                       first_name: 'This',
-                       last_name: 'Guy',
-                       user_type: 'customer',
-                       organization_id: 1,
-                       password: 'password')
-    User.count.should eql(users)
+    @user.delete(:password_confirmation)
+    user = User.create(@user)
+    User.count.should eql(@users)
   end
 
   it "outputs a hash of restaurants User ordered from" do
-    user = User.create(username: 'person6@email.com',
-                       first_name: 'This',
-                       last_name: 'Guy',
-                       user_type: 'customer',
-                       organization_id: 1,
-                       password: 'password',
-                       password_confirmation: 'password')
+    user = User.create(@user)
     user.restaurant_info.should be_a(Hash)
   end
 
   it "outputs a hash of items User has ordered" do
-    user = User.create(username: 'person7@email.com',
-                       first_name: 'This',
-                       last_name: 'Guy',
-                       user_type: 'customer',
-                       organization_id: 1,
-                       password: 'password',
-                       password_confirmation: 'password')
+    user = User.create(@user)
     user.item_info.should be_a(Hash)
   end
 
